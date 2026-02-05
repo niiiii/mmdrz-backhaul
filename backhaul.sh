7b2d3b47346b2ef2ec278c85f23cc7be1828545d
mmdrz hbz <mohammadreza.h1381@gmail.com>
2026-02-05 22:37:16 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -130,10 +130,10 @@ download_and_extract_backhaul() {
     ARCH=$(uname -m)
     case "$ARCH" in
         x86_64)
-            DOWNLOAD_URL="https://mm-gozar.top/script/backhaul_amd64.tar.gz"
+            DOWNLOAD_URL="https://raw.githubusercontent.com/niiiii/mmdrz-backhaul/refs/heads/main/backhaul_amd64.tar.gz"
             ;;
         arm64|aarch64)
-            DOWNLOAD_URL="https://mm-gozar.top/script/backhaul_arm64.tar.gz"
+            DOWNLOAD_URL="https://raw.githubusercontent.com/niiiii/mmdrz-backhaul/refs/heads/main/backhaul_arm64.tar.gz"
             ;;
         *)
             echo -e "${RED}Unsupported architecture: $ARCH.${NC}"
